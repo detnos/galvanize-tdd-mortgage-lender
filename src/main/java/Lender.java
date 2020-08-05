cdc6c34f78264cafa87821a9ab0cf9838a0cef14
Hugh <det.nosnibor00@gmail.com>
2020-08-05 13:58:07 -0500

    instructions followed.

--- src/main/java/Lender.java
+++ src/main/java/Lender.java
@@ -1,3 +1,15 @@
 final class Lender {
 
+    private long availableFunds = 0;
+
+    public long getAvailableFunds() {
+        return this.availableFunds;
+    }
+
+    void addFunds(final long funds) {
+        if(funds <= 0) {
+            throw new IllegalArgumentException("Amount added must be greater than zero.");
+        }
+        this.availableFunds = Math.addExact(this.availableFunds, funds);
+    }
 }
--- src/test/java/LenderTest.java
+++ src/test/java/LenderTest.java
@@ -5,4 +5,107 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 final class LenderTest {
 
+    @Test
+    void getAvailableFundsReturnsZeroByDefault() {
+        //Setup
+        final Lender lender = new Lender();
+        final long expected = 0L;
+
+        //Exercise
+        final long actual = lender.getAvailableFunds();
+
+        //Assert
+        assertThat(actual).isEqualTo(expected);
+
+        //Teardown
+    }
+
+    @Test
+    void addFundsThrowsAnExceptionForZero(){
+        //Setup
+        final Lender lender = new Lender();
+        final String expected = "Amount added must be greater than zero.";
+
+        //Exercise
+        final Exception actual = assertThrows(IllegalArgumentException.class,
+                () -> lender.addFunds(0L));
+
+        //Assert
+        assertThat(actual.getMessage()).isEqualTo(expected);
+    }
+
+    @Test
+    void addFundsIncreasesAvailableFundsByOne(){
+        //Setup
+        final Lender lender = new Lender();
+        final long expected = 1L;
+
+        //Exercise
+        lender.addFunds(expected);
+
+        final long actual = lender.getAvailableFunds();
+
+        //Assert
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void addFundsThrowsAnExceptionForNegativeValue(){
+        //Setup
+        final Lender lender = new Lender();
+        final String expected = "Amount added must be greater than zero.";
+
+        //Exercise
+        final Exception actual = assertThrows(IllegalArgumentException.class,
+                () -> lender.addFunds(Long.MIN_VALUE));
+
+        //Assert
+        assertThat(actual.getMessage()).isEqualTo(expected);
+    }
+
+    @Test
+    void addFundsIncreasesAvailableFundsByTwo(){
+        //Setup
+        final Lender lender = new Lender();
+        final long expected = 2L;
+
+        //Exercise
+        lender.addFunds(expected);
+
+        final long actual = lender.getAvailableFunds();
+
+        //Assert
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void addFundsIncreasesAvailableFundsByThreeThenFour(){
+        //Setup
+        final Lender lender = new Lender();
+        final long expected = 7L;
+
+        //Exercise
+        lender.addFunds(3L);
+        lender.addFunds(4L);
+
+        final long actual = lender.getAvailableFunds();
+
+        //Assert
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void addFundsThrowExceptionIfOverflowWillHappen(){
+        //Setup
+        final Lender lender = new Lender();
+        lender.addFunds(Long.MAX_VALUE);
+        final String expected = "long overflow";
+
+        //Exercise
+        final Exception actual = assertThrows(ArithmeticException.class,
+                () -> lender.addFunds(1L));
+
+        //Assert
+        assertThat(actual.getMessage()).isEqualTo(expected);
+    }
 }
